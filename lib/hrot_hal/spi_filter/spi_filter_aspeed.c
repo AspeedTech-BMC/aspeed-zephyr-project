6015fa217a8c198ee2a927c3e3d902b90041f00f
Troy Lee <troy_lee@aspeedtech.com>
2022-05-19 03:57:16 +0000

    Fixes unbinded device causing system halt
    Fail to bind device should not halt the system.
    
    Signed-off-by: Troy Lee <troy_lee@aspeedtech.com>
    Change-Id: If5e57bddf8250c534245bc4b206653b6c851d9c6

--- lib/hrot_hal/spi_filter/spi_filter_aspeed.c
+++ lib/hrot_hal/spi_filter/spi_filter_aspeed.c
@@ -18,6 +18,10 @@ void SPI_Monitor_Enable(char *dev_name, bool enabled)
 	const struct device *dev_m = NULL;
 
 	dev_m = device_get_binding(dev_name);
+	if (dev_m == NULL) {
+		printk("%s: unable to bind %s\n", __FUNCTION__, dev_name);
+		return ;
+	}
 	spim_rst_flash(dev_m, 1000);
 	spim_passthrough_config(dev_m, 0, false);
 #if 0
@@ -36,6 +40,10 @@ int Set_SPI_Filter_RW_Region(char *dev_name, enum addr_priv_rw_select rw_select,
 	const struct device *dev_m = NULL;
 
 	dev_m = device_get_binding(dev_name);
+	if (dev_m == NULL) {
+		printk("%s: unable to bind %s\n", __FUNCTION__, dev_name);
+		return -1;
+	}
 	ret = spim_address_privilege_config(dev_m, rw_select, op, addr, len);
 
 	return ret;
