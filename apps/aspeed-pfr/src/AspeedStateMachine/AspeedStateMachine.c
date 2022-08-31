AspeedTech-BMC/aspeed-zephyr-project

commit a2de142d1834326d26dbc0abb2741f73049818fa

Bypass I2C Filter in unprovision mode
In UNPROVISION state, SPI Monitor and I2C Filter should be disable.

Signed-off-by: Troy Lee <troy_lee@aspeedtech.com>
Change-Id: Ia49e338ead0aa9fe821a6e88ebdbd92e539b73b0

apps/aspeed-pfr/src/AspeedStateMachine/AspeedStateMachine.c
@@ -489,6 +489,33 @@ void do_rot_recovery(void *o)
 	LOG_DBG("End");
 }
 
+void i2c_filter_release(const char *i2cf)
+{
+	const struct device *i2c_filter = device_get_binding(i2cf);
+	int ret;
+	if (i2c_filter) {
+		ret = ast_i2c_filter_init(i2c_filter);
+		if (ret) {
+			LOG_ERR("ast_i2c_filter_init(%s) ret=%d", i2cf, ret);
+		}
+
+		/*
+		 * dev: pointer to device structure
+		 * filter_en: value to enable filter device
+		 * wlist_en: value to enable white list
+		 * clr_idx: value to clear index table
+		 * clr_tbl: value to clear white list table
+		 */
+		ret = ast_i2c_filter_en(i2c_filter, true, false, false, false);
+		if (ret) {
+			LOG_ERR("ast_i2c_filter_en(%s) ret=%d", i2cf, ret);
+		}
+		LOG_INF("Bypass %s", i2cf);
+	} else {
+		LOG_ERR("Fail to find %s device, skipped", i2cf);
+	}
+}
+
 void enter_tzero(void *o)
 {
 	LOG_DBG("Start");
@@ -531,18 +558,65 @@ void enter_tzero(void *o)
 			LOG_ERR("Host firmware is invalid, host won't boot");
 	} else {
 		/* Unprovisioned - Releasing System Reset */
-		Set_SPI_Filter_RW_Region("spi_m1", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		Set_SPI_Filter_RW_Region("spi_m1", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		SPI_Monitor_Enable("spi_m1", false);
-		Set_SPI_Filter_RW_Region("spi_m2", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		Set_SPI_Filter_RW_Region("spi_m2", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		SPI_Monitor_Enable("spi_m2", false);
-		Set_SPI_Filter_RW_Region("spi_m3", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		Set_SPI_Filter_RW_Region("spi_m3", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		SPI_Monitor_Enable("spi_m3", false);
-		Set_SPI_Filter_RW_Region("spi_m4", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		Set_SPI_Filter_RW_Region("spi_m4", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
-		SPI_Monitor_Enable("spi_m4", false);
+		if (device_get_binding("spi_m1")!=NULL) {
+			Set_SPI_Filter_RW_Region("spi_m1", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			Set_SPI_Filter_RW_Region("spi_m1", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			SPI_Monitor_Enable("spi_m1", false);
+			LOG_INF("Bypass %s", "spi_m1");
+		}
+
+		if (device_get_binding("spi_m2") != NULL) {
+			Set_SPI_Filter_RW_Region("spi_m2", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			Set_SPI_Filter_RW_Region("spi_m2", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			SPI_Monitor_Enable("spi_m2", false);
+			LOG_INF("Bypass %s", "spi_m2");
+		}
+
+		if (device_get_binding("spi_m3") != NULL) {
+			Set_SPI_Filter_RW_Region("spi_m3", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			Set_SPI_Filter_RW_Region("spi_m3", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			SPI_Monitor_Enable("spi_m3", false);
+			LOG_INF("Bypass %s", "spi_m3");
+		}
+
+		if (device_get_binding("spi_m4") != NULL) {
+			Set_SPI_Filter_RW_Region("spi_m4", SPI_FILTER_READ_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			Set_SPI_Filter_RW_Region("spi_m4", SPI_FILTER_WRITE_PRIV, SPI_FILTER_PRIV_ENABLE, 0, 0x10000000);
+			SPI_Monitor_Enable("spi_m4", false);
+			LOG_INF("Bypass %s", "spi_m4");
+		}
+
+		/* Releasing I2C Filter */
+		const struct device *dev = NULL;
+		if ( (dev = device_get_binding("I2C_FILTER_0")) != NULL) {
+			ast_i2c_filter_init(dev);
+			ast_i2c_filter_en(dev, true, false, false, false);
+			LOG_INF("Bypass %s", dev->name);
+		}
+		
+		if ( (dev = device_get_binding("I2C_FILTER_1")) != NULL) {
+			ast_i2c_filter_init(dev);
+			ast_i2c_filter_en(dev, true, false, false, false);
+			LOG_INF("Bypass %s", dev->name);
+		}
+		
+		if ( (dev = device_get_binding("I2C_FILTER_2")) != NULL) {
+			ast_i2c_filter_init(dev);
+			ast_i2c_filter_en(dev, true, false, false, false);
+			LOG_INF("Bypass %s", dev->name);
+		}
+		
+		if ( (dev = device_get_binding("I2C_FILTER_3")) != NULL) {
+			ast_i2c_filter_init(dev);
+			ast_i2c_filter_en(dev, true, false, false, false);
+			LOG_INF("Bypass %s", dev->name);
+		}
+
+		if ( (dev = device_get_binding("I2C_FILTER_4")) != NULL) {
+			ast_i2c_filter_init(dev);
+			ast_i2c_filter_en(dev, true, false, false, false);
+			LOG_INF("Bypass %s", dev->name);
+		}
 
 		BMCBootRelease();
 		PCHBootRelease();

apps/aspeed-pfr/src/platform_monitor/spim_monitor.c
@@ -111,7 +111,7 @@ void spim_irq_init(void)
 	for (i = 0; i < 3; i++) {
 		spim_devs[i] = device_get_binding(spim_dev_names[i]);
 		if (!spim_devs[i]) {
-			LOG_ERR("demo_err: cannot get device, %s.\n", spim_dev_names[i]);
+			LOG_ERR("Cannot get device, %s, skipped.", spim_dev_names[i]);
 			continue;
 		}
 		log_ctrls[i].dev = spim_devs[i];

commit 14e2e027ecdb8079ebf3a11fcc18bd81756f2047

Remove dead code and missing header
Fixes: a2de142d1834 ("Missing header and having duplicated code")

Signed-off-by: Troy Lee <troy_lee@aspeedtech.com>
Change-Id: Ia9807e180e61d1669c36588dbc27a560f8e81c55

apps/aspeed-pfr/src/AspeedStateMachine/AspeedStateMachine.c
@@ -9,6 +9,7 @@
 #include <logging/log.h>
 #include <logging/log_ctrl.h>
 #include <drivers/i2c/pfr/swmbx.h>
+#include <drivers/i2c/pfr/i2c_filter.h>
 #include <drivers/misc/aspeed/abr_aspeed.h>
 #include <drivers/flash.h>
 
@@ -487,33 +488,6 @@ void do_rot_recovery(void *o)
 		GenerateStateMachineEvent(RECOVERY_FAILED, NULL);
 	}
 	LOG_DBG("End");
-}
-
-void i2c_filter_release(const char *i2cf)
-{
-	const struct device *i2c_filter = device_get_binding(i2cf);
-	int ret;
-	if (i2c_filter) {
-		ret = ast_i2c_filter_init(i2c_filter);
-		if (ret) {
-			LOG_ERR("ast_i2c_filter_init(%s) ret=%d", i2cf, ret);
-		}
-
-		/*
-		 * dev: pointer to device structure
-		 * filter_en: value to enable filter device
-		 * wlist_en: value to enable white list
-		 * clr_idx: value to clear index table
-		 * clr_tbl: value to clear white list table
-		 */
-		ret = ast_i2c_filter_en(i2c_filter, true, false, false, false);
-		if (ret) {
-			LOG_ERR("ast_i2c_filter_en(%s) ret=%d", i2cf, ret);
-		}
-		LOG_INF("Bypass %s", i2cf);
-	} else {
-		LOG_ERR("Fail to find %s device, skipped", i2cf);
-	}
 }
 
 void enter_tzero(void *o)