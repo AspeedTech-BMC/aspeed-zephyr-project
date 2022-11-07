e1bb81e2ac07254cf0d15f15cb898b9763a5ab72
Jamin Lin <jamin_lin@aspeedtech.com>
2022-11-07 15:50:09 +0800

    mctp: update mctp test command
    update shell test command to test large message
    
    Signed-off-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Change-Id: I5fa51b9ca8026be81b160a9967619890703e41f8

--- apps/aspeed-pfr/src/mctp/test_mctp_shell.c
+++ apps/aspeed-pfr/src/mctp/test_mctp_shell.c
@@ -12,8 +12,9 @@
 #include "mctp_utils.h"
 #include "plat_mctp.h"
 
-static uint8_t request_data[256] = {0};
-static uint8_t message[512] = {0};
+#define MCTP_TEST_DEBUG 0
+static uint8_t request_buf[MCTP_BASE_PROTOCOL_MAX_MESSAGE_BODY] = {0};
+static uint8_t message_buf[MCTP_BASE_PROTOCOL_MAX_MESSAGE_LEN] = {0};
 
 static int cmd_mctp_send_msg(const struct shell *shell, size_t argc, char **argv)
 {
@@ -40,23 +41,24 @@ static int cmd_mctp_send_msg(const struct shell *shell, size_t argc, char **argv
 	}
 
 	mctp_interface = &mctp_inst->mctp_wrapper.mctp_interface;
-	// request data
-	// request_data[0] =message type
-	// request_data[1] = rq
-	// request_data[2] = command code
-	memset(request_data, 0, sizeof(request_data));
+	// request
+	// request_buf[0] = message type
+	// request_buf[1] = rq
+	// request_buf[2] = command code
+	memset(request_buf, 0, sizeof(request_buf));
 	for (i = 0; i < req_len; i++)
-		request_data[i] = strtol(argv[argc_req_idx++], NULL, 16);
+		request_buf[i] = strtol(argv[argc_req_idx++], NULL, 16);
 
-	shell_info(shell, "req_len = %d", req_len);
+#if MCTP_TEST_DEBUG
+	shell_print(shell, "req_len = %d", req_len);
 	shell_print(shell, "request:");
-	shell_hexdump(shell, request_data, req_len);
-
+	shell_hexdump(shell, request_buf, req_len);
+#endif
 	status = mctp_interface_issue_request(mctp_interface, &mctp_inst->mctp_cmd_channel,
-		dst_addr, dst_eid, request_data, req_len, message, sizeof(message), 1000);
+		dst_addr, dst_eid, request_buf, req_len, message_buf, sizeof(message_buf), 1000);
 
 	if (status != 0)
-		shell_error(shell, "mctp issue request failed");
+		shell_print(shell, "mctp issue request failed(%x)", status);
 	else {
 		shell_print(shell, "response:");
 		shell_hexdump(shell, mctp_interface->req_buffer.data, mctp_interface->req_buffer.length);
@@ -85,9 +87,8 @@ static int cmd_mctp_send_large_msg(const struct shell *shell, size_t argc, char 
 	dst_eid = strtol(argv[3], NULL, 16);
 	count = strtol(argv[7], NULL, 10);
 
-	shell_print(shell, "cound =%d", count);
-	if (count > sizeof(request_data)) {
-		shell_print(shell, "count too large");
+	if ((count + 3) > sizeof(request_buf)) {
+		shell_print(shell, "count(%d) is too big", count);
 		return 0;
 	}
 
@@ -98,29 +99,29 @@ static int cmd_mctp_send_large_msg(const struct shell *shell, size_t argc, char 
 	}
 
 	mctp_interface = &mctp_inst->mctp_wrapper.mctp_interface;
-	// request data
-	// request_data[0] =message type
-	// request_data[1] = rq
-	// request_date[2] = command code
-	memset(request_data, 0, sizeof(request_data));
-	request_data[0] = strtol(argv[4], NULL, 16);
-	request_data[1] = strtol(argv[5], NULL, 16);
-	request_data[2] = strtol(argv[6], NULL, 16);
-
+	// request
+	// request_buf[0] = message type
+	// request_buf[1] = rq
+	// requaet_buf[2] = command code
+	memset(request_buf, 0, sizeof(request_buf));
+	request_buf[0] = strtol(argv[4], NULL, 16);
+	request_buf[1] = strtol(argv[5], NULL, 16);
+	request_buf[2] = strtol(argv[6], NULL, 16);
 
 	for (i = 0; i < count; i++)
-		request_data[argc_req_idx++] = i % 0x100;
+		request_buf[argc_req_idx++] = i % 0x100;
 
 	req_len = 3 + count;
-	shell_info(shell, "req_len = %d", req_len);
+#if MCTP_TEST_DEBUG
+	shell_print(shell, "req_len = %d", req_len);
 	shell_print(shell, "request:");
-	shell_hexdump(shell, request_data, req_len);
-
+	shell_hexdump(shell, request_buf, req_len);
+#endif
 	status = mctp_interface_issue_request(mctp_interface, &mctp_inst->mctp_cmd_channel,
-		dst_addr, dst_eid, request_data, req_len, message, sizeof(message), 1000);
+		dst_addr, dst_eid, request_buf, req_len, message_buf, sizeof(message_buf), 1000);
 
 	if (status != 0)
-		shell_error(shell, "mctp issue request failed");
+		shell_error(shell, "mctp issue request failed(%x)", status);
 	else {
 		shell_print(shell, "response:");
 		shell_hexdump(shell, mctp_interface->req_buffer.data, mctp_interface->req_buffer.length);
