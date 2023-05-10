3fc195ab781a1bc73aff11823398708909bd760a
Neal Liu <neal_liu@aspeedtech.com>
2023-05-10 17:01:15 +0800

    preload-fw: otp: check secure boot status from SEC instead
    Check secure boot status from SEC instead of OTPCFG.
    
    Signed-off-by: Neal Liu <neal_liu@aspeedtech.com>
    Change-Id: I6261e017c056b09931deb79d841487a1ac118783

--- apps/preload-fw/src/otp/otp_utils.c
+++ apps/preload-fw/src/otp/otp_utils.c
@@ -50,17 +50,12 @@ static uint32_t otp_header_buf[OTP_HEADER_LENGTH];
 
 bool is_otp_secureboot_en(enum otp_status *otp_rc)
 {
-	bool is_secureboot_en = false;
-	uint32_t otp_conf_val;
+	uint32_t val;
 
-	*otp_rc = aspeed_otp_read_conf(OTP_CONF0, &otp_conf_val, 1);
-	if (*otp_rc) {
-		LOG_ERR("Failed to get secure boot status");
-		return is_secureboot_en;
-	}
+	val = sys_read32(SEC_STATUS);
+	*otp_rc = 0;
 
-
-	return (otp_conf_val & OTP_CONF0_SECURE_BOOT_EN) ? true :  false;
+	return (val & SEC_STATUS_SECURE_BOOT_EN) ? true : false;
 }
 
 int otp_append_vault_key(uint32_t *vault_key)
