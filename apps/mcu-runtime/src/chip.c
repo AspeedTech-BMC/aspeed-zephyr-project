94349cf7bb1f95c282ae850d8ecaacb5e4e63ba5
Cool Lee <cool_lee@aspeedtech.com>
2025-09-23 11:43:28 +0800

    FPGA: Update SS MCU Rom
    Update ssmcu rom
    
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: I55681b4f2c7cbcefcec4c6d57258bec5c63ffc28

--- apps/mcu-runtime/src/chip.c
+++ apps/mcu-runtime/src/chip.c
@@ -22,6 +22,49 @@
 
 LOG_MODULE_REGISTER(ast_chip, CONFIG_SOC_FMC_LOG_LEVEL);
 
+#ifdef CONFIG_ASPEED_HAPS
+#define AST2700 0
+#define AST2755 1
+#define AST27XX  AST2755
+static int bootrom_init(struct ast_chip *chip)
+{
+	uint32_t i;
+	uint32_t *rom = (uint32_t *)0x20300000;
+	uint32_t rom_base[] = {0x14b00000, 0x14c40000};
+	uint32_t rom_size[] = {0x12000, 0x8000};
+	uint32_t base;
+
+	LOG_DBG("bootrom_init\n");
+
+#if defined(AST2755)
+	sys_write32(0x0, 0x14c023d8); // map to caliptra ss 0x00000000.
+#endif
+
+	base = rom_base[AST27XX];
+	for (i = 0; i < rom_size[AST27XX]/4; i++) {
+	      if (i < 8)
+		      LOG_DBG("rom[%d]=0x%x\n", i, rom[i]);
+
+	      sys_write32(rom[i], (base + 4 * i));
+	}
+
+	return 0;
+}
+
+static struct peripheral peri_tbl[] = {
+	{"POLICY",	NULL, NULL},//sys_policy_init},
+	{"WDT",		wdt_init, NULL},
+	{"EXTRST",	extrst_mask_init, NULL},
+	{"LOADER",	ast_loader_init, NULL},
+	{"BOOTROM",     bootrom_init, NULL},
+//	{"SLI1",	sli_init_f, NULL},
+//	{"DP",		dp_init, NULL},
+//	{"SLI0",	sli_init_r, NULL},
+	{"DRAM",	dram_init, NULL},
+//	{"PCI",		pci_init, NULL},
+//	{"USB",		usb_init, NULL},
+};
+#else
 static struct peripheral peri_tbl[] = {
 	{"POLICY",	NULL, NULL},//sys_policy_init},
 	{"WDT",		wdt_init, NULL},
@@ -34,6 +77,7 @@ static struct peripheral peri_tbl[] = {
 	{"PCI",		pci_init, NULL},
 	{"USB",		usb_init, NULL},
 };
+#endif
 
 static struct ast_chip ast_27xx = {
 	.rev_id = 0,
