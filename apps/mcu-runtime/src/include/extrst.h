3e70345db9b8e3c9b5864891e287490ec425b3e4
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2025-09-25 15:24:58 +0800

    mcu runtime: Enable SSP/TSP reset bits in reset mask
    When EXTRST# reset is triggered, SSP and TSP coprocessors
    will be reset by default. This behavior is also applied to
    WDT reset.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I1fb3946c372f1dc044cc144126fbdf5741ca2fc4

--- apps/mcu-runtime/src/include/extrst.h
+++ apps/mcu-runtime/src/include/extrst.h
@@ -8,7 +8,7 @@
 
 #include <chip.h>
 
-#define SCU0_EXTRST_MASK_1_VAL  0x8207e771
+#define SCU0_EXTRST_MASK_1_VAL  0x8207ff71
 #define SCU0_EXTRST_MASK_2_VAL  0x000003f6
 
 #define SCU1_EXTRST_MASK_1_VAL  0x000093ec
