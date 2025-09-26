43b167508be86b64cfe16a57b8ebf9d42c8f9475
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2025-09-26 18:36:05 +0800

    mcu_rt: Add support from uart ymodem protocol
    - Add Ymodem protocol for UART path to support
      recovery from UART.
    - Update recovery from UART callback implementation.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I293207a3351d0f898d92796b3ac65d24b850bf4e

---
+++ apps/mcu-runtime/lib/xyzmodem/yModem.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025 ASPEED Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _XYZMODEM_H_
+#define _XYZMODEM_H_
+
+struct ymodem_port {
+	/* Blocking write of len bytes to the transport (e.g., UART). */
+	void (*write)(char c);
+
+	/* Read up to len bytes with timeout_ms.
+	 * Return bytes read (0 on timeout), <0 on error.
+	 */
+	int (*read)(uint8_t *buf, uint32_t len, uint32_t timeout_ms);
+
+	/* Maximum timeouts/retries (tunable). */
+	uint32_t rx_timeout_ms;	/* default 1000 ms */
+	uint8_t max_retries;	/* default 10 */
+};
+
+enum ymodem_status {
+	YMODEM_OK = 0,
+	YMODEM_ERR_IO = -1,
+	YMODEM_ERR_PROTO = -2,
+	YMODEM_ERR_CRC = -3,
+	YMODEM_ERR_ABORT = -4,
+	YMODEM_ERR_NOSPACE = -5,
+};
+
+int readc(uint8_t *buf, uint32_t len, uint32_t timeout_ms);
+void writec(char y);
+enum ymodem_status ymodem_receive_into(struct ymodem_port *port,
+				       uint8_t *buf,
+				       uint32_t *out_size,
+				       char *out_name, uint32_t name_cap);
+int ymodem_open(struct device *dev);
+int ymodem_close(void);
+
+#endif /* _XYZMODEM_H_ */
--- apps/mcu-runtime/src/uart.c
+++ apps/mcu-runtime/src/uart.c
@@ -3,16 +3,24 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <ast_loader.h>
 #include <platform.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
+#include <yModem.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/logging/log.h>
-#include <scu_ast2700.h>
-#include <ast_loader.h>
 
 LOG_MODULE_REGISTER(ast_uart, CONFIG_SOC_FMC_LOG_LEVEL);
+
+struct ymodem_port uport = {
+	.write = writec,
+	.read = readc,
+	.rx_timeout_ms = 3000,
+	.max_retries = 20,
+};
 
 static int uart_init(struct device *dev)
 {
@@ -21,8 +29,23 @@ static int uart_init(struct device *dev)
 
 static int uart_load(struct device *dev, uint32_t *dst, uint32_t *len)
 {
+	uint32_t out_sz = 0, i;
+	enum ymodem_status sts = YMODEM_OK;
 
-	return 0;
+	if (!dev)
+		return -1;
+
+	ymodem_open(dev);
+
+	sts = ymodem_receive_into(&uport, (uint8_t *)dst, &out_sz, NULL, 0);
+	if (sts == YMODEM_OK)
+		*len = out_sz;
+	else
+		*len = 0;
+
+	ymodem_close();
+
+	return (int)sts;
 }
 
 static struct ast_loader_ops bootuart_ops = {
@@ -34,9 +57,9 @@ int uart_register(struct ast_loader *loader)
 {
 	struct device *dev;
 
-	dev = (struct device *)device_get_binding("uart@xxxxxxxx");
+	dev = (struct device *)device_get_binding("serial@14c33b00");
 	if (!dev) {
-		LOG_ERR("No device named uart");
+		LOG_ERR("Cannot bind uart device correctly");
 		return -1;
 	}
 
