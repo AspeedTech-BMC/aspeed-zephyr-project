d64819851f11af61ed88ab23fb423a3e0ade6822
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2025-11-05 20:03:07 +0800

    mcu-runtime: spi: Support single flash ABR mode
    Add support for single flash ABR mode in the SPI loader.
    When ABR is enabled and single flash mode is detected,
    the image source offset is adjusted by adding half of
    the physical flash size.
    
    This ensures the correct image is loaded from the backup
    region in single flash configurations.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: Ib535c930a1f4c86d3e24c09b937025a2b955fba5

--- apps/mcu-runtime/src/include/spi.h
+++ apps/mcu-runtime/src/include/spi.h
@@ -10,12 +10,19 @@
  * be used.
  */
 
-#ifndef _SPI_AST2700_H
-#define _SPI_AST2700_H
+#ifndef _SPI_AST2700_H_
+#define _SPI_AST2700_H_
 
 #include <stdlib.h>
 #include <ast_loader.h>
 
-//int spi_init(struct device *dev);
-//int spi_copy(struct device *dev, uint32_t *dest, uint32_t src, uint32_t len);
+#define SPI_SZ_UNSET		0x0
+#define SPI_SZ_8MB		0x800000
+#define SPI_SZ_16MB		0x1000000
+#define SPI_SZ_32MB		0x2000000
+#define SPI_SZ_64MB		0x4000000
+#define SPI_SZ_128MB		0x8000000
+#define SPI_SZ_256MB		0x10000000
+#define SPI_SZ_512MB		0x20000000
+
 #endif
--- apps/mcu-runtime/src/spi.c
+++ apps/mcu-runtime/src/spi.c
@@ -20,7 +20,9 @@
 #include <zephyr/drivers/flash.h>
 #include <zephyr/drivers/spi_nor.h>
 #include <zephyr/sys/util.h>
+#include <abr.h>
 #include <ast_loader.h>
+#include <spi.h>
 
 #define ASPEED_FMC_REG_BASE	0x14000000
 #define INTR_CTRL		(ASPEED_FMC_REG_BASE + 0x008)
@@ -41,12 +43,73 @@
 
 LOG_MODULE_REGISTER(ast_spi, CONFIG_SOC_FMC_LOG_LEVEL);
 
+/*
+ * SCU flash size: SCU030[15:13]
+ * 7: 512MB
+ * 6: 256MB
+ * 5: 128MB
+ * 4: 64MB
+ * 3: 32MB
+ * 2: 16MB
+ * 1: 8MB
+ * 0: disabled
+ */
+static uint32_t spi_get_flash_sz_strap(void)
+{
+	uint32_t scu_flash_sz;
+	uint32_t flash_sz_phy = SPI_SZ_256MB;
+
+	scu_flash_sz = (sys_read32(SCU1_REG + 0x030) >> 13) & 0x7;
+	switch (scu_flash_sz) {
+	case 0x7:
+		flash_sz_phy = SPI_SZ_512MB;
+		break;
+	case 0x6:
+		flash_sz_phy = SPI_SZ_256MB;
+		break;
+	case 0x5:
+		flash_sz_phy = SPI_SZ_128MB;
+		break;
+	case 0x4:
+		flash_sz_phy = SPI_SZ_64MB;
+		break;
+	case 0x3:
+		flash_sz_phy = SPI_SZ_32MB;
+		break;
+	case 0x2:
+		flash_sz_phy = SPI_SZ_16MB;
+		break;
+	case 0x1:
+		flash_sz_phy = SPI_SZ_8MB;
+		break;
+	case 0x0:
+		flash_sz_phy = SPI_SZ_UNSET;
+		break;
+	default:
+		flash_sz_phy = SPI_SZ_UNSET;
+	}
+
+	return flash_sz_phy;
+}
+
+static bool spi_single_flash_abr(void)
+{
+	return !!(sys_read32(SCU1_REG + 0x030) & BIT(29));
+}
+
 static int spi_copy(struct device *dev, uint32_t *dest, uint32_t src, uint32_t len)
 {
 	int ret;
 
 	if (!dev)
 		return -1;
+
+	if (abr_get_ind() &&
+	    spi_single_flash_abr()) {
+	    	LOG_DBG("single flash abr with size: 0x%x",
+	    		spi_get_flash_sz_strap());
+		src += spi_get_flash_sz_strap() / 2;
+	}
 
 	LOG_DBG("dest: %08x, src: %08x, len:%08x\n", (uint32_t)dest, src, len);
 	ret = flash_read(dev, (off_t)src, (void *)dest, (size_t)len);
