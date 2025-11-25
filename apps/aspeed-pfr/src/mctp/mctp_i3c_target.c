adafd388593825ba89db5f847950234b8ed03f51
Steven Lee <steven_lee@aspeedtech.com>
2025-11-25 15:03:08 +0800

    mctp_i3c_target: Fix incorrect dest_eid initialization
    The destination EID was not initialized properly, leading to incorrect
    routing behavior.
    
    Signed-off-by: Steven Lee <steven_lee@aspeedtech.com>
    Change-Id: I7fa855eb0b5542ccc2bcadb7a81662d50b7f2a7d

--- apps/aspeed-pfr/src/mctp/mctp_i3c_target.c
+++ apps/aspeed-pfr/src/mctp/mctp_i3c_target.c
@@ -229,6 +229,7 @@ void mctp_i3c_target_intf_init(void)
 		}
 		mctp_instance->medium_conf.i3c_conf.bus = i3c_dev_p->i3c_conf.bus;
 		mctp_instance->medium_conf.i3c_conf.addr = i3c_dev_p->i3c_conf.addr;
+		mctp_instance->medium_conf.i3c_conf.dest_eid = i3c_dev_p->i3c_conf.dest_eid;
 		mctp_channel_id = CMD_CHANNEL_I3C_BASE | CMD_CHANNEL_I3C_TARGET |
 			mctp_instance->medium_conf.i3c_conf.bus;
 		rc = cmd_channel_mctp_init(&mctp_instance->mctp_cmd_channel,
