4905721af5e36caeaa1804d1e8788a1b70e9d96d
Cool Lee <cool_lee@aspeedtech.com>
2026-01-06 10:04:51 +0800

    mcu-runtime: ast1040: Rom update
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: I0457e0fdc0acb7681238f2ac464186d19ce3801e

--- apps/mcu-runtime/soc/ast1040/chip_ast1040.c
+++ apps/mcu-runtime/soc/ast1040/chip_ast1040.c
@@ -16,12 +16,47 @@
 
 LOG_MODULE_REGISTER(ast_chip, CONFIG_SOC_FMC_LOG_LEVEL);
 
+#ifdef CONFIG_ASPEED_HAPS
+#define AST2700 0
+#define AST1040 1
+#define AST27XX  AST1040
+static int bootrom_init(struct ast_chip *chip)
+{
+	uint32_t i;
+	uint32_t *rom = (uint32_t *)0x20300000;
+	uint32_t ssrom_base[] = {0x14b00000, 0x14c40000};
+	uint32_t ssrom_size[] = {0x12000, 0x8000};
+	uint32_t base;
+
+	LOG_DBG("bootrom_init\n");
+
+#if defined(AST1040)
+	sys_write32(0x0, 0x14c023d4); // map to caliptra ss 0x00000000.
+#endif
+
+	base = ssrom_base[AST27XX];
+
+	rom = (uint32_t *) 0x20700000;
+
+	for (i = 0; i < 0x30000/4; i++) {
+	      if (i < 8)
+		      LOG_INF("bootmcu rom[%d]=0x%x\n", i, rom[i]);
+
+	      sys_write32(rom[i], (0x14b00000 + 4 * i));
+	}
+
+	return 0;
+}
+
 static struct peripheral peri_tbl[] = {
-	{"POLICY",	NULL, NULL},//sys_policy_init},
-	{"WDT",		wdt_init, NULL},
-	// {"EXTRST",	extrst_mask_init, NULL},
+	{"LOADER",	ast_loader_init, NULL},
+	{"BOOTROM",     bootrom_init, NULL},
+};
+#else
+static struct peripheral peri_tbl[] = {
 	{"LOADER",	ast_loader_init, NULL},
 };
+#endif
 
 static struct ast_chip ast_1040 = {
 	.rev_id = 0,
