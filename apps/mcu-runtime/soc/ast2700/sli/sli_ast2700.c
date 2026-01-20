02218c5264eb48776d60dca123fea5f126c60807
Jammy Huang <jammy_huang@aspeedtech.com>
2026-01-20 10:48:10 +0800

    revert "sli: Add reset flow"
    This patch is not helpful confirmed from the customer side.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Change-Id: Id0722a9c44af5b727b3fb6dc1a54cf4c4f9b1e89

--- apps/mcu-runtime/soc/ast2700/sli/sli_ast2700.c
+++ apps/mcu-runtime/soc/ast2700/sli/sli_ast2700.c
@@ -159,18 +159,6 @@ struct sli_data {
 #define SCU0_SCRATCH31_SLI1_READY	BIT(0)
 #define AHBC_MAX_TIMEOUT		0x1ff
 
-static void trigger_reset(void)
-{
-	// Add SLI + DRAM reset
-	setbits_le32((void *)0x14c3701c, BIT(2) | BIT(1));
-	setbits_le32((void *)0x14c37028, BIT(3));
-
-	// trigger reset
-	sys_write32(0x200, (void *)0x14c37004);
-	sys_write32(0x4755, (void *)0x14c37008);
-	sys_write32(0x13, (void *)0x14c3700c);
-}
-
 static bool is_sli_calibrated(struct sli_data *data)
 {
 	uint32_t reg_val;
@@ -539,11 +527,6 @@ static void sli_calibrate_mbus_delay(struct sli_data *data, bool is_DS, bool is_
 		if ((d_last_pass - d_first_pass) >= 3)
 			break;
 		LOG_DBG("%s SLIM %s win: {%d, %d} retry %d\n", die_name, dir, d_first_pass, d_last_pass, count);
-	}
-
-	if (count == SLIM_RETRY_COUNT) {
-		LOG_WRN("%s SLIM %s calibration failed, {%d, %d}\n", die_name, dir, d_first_pass, d_last_pass);
-		trigger_reset();
 	}
 
 	dc = (d_first_pass + d_last_pass) >> 1;
