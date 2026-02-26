eaf0f25ca03ed98820424667e9ac75c802405e70
Kane-Chen-AS <kane_chen@aspeedtech.com>
2026-02-26 16:33:05 +0800

    board: Remove AST2700A0 board
    AST2700A0 is no longer supported. Remove this board type.
    
    Signed-off-by: Kane-Chen-AS <kane_chen@aspeedtech.com>
    Change-Id: Icc9eccfb176d04e6ea41cfd757cf520d6a3459a0

--- apps/aspeed-pfr/src/mctp/plat_mctp.h
+++ apps/aspeed-pfr/src/mctp/plat_mctp.h
@@ -9,11 +9,7 @@
 #include "mctp.h"
 
 /* i2c dev bus */
-#if defined(CONFIG_BOARD_AST2700A0_DCSCM)
-#define I2C_BUS_BMC 0x05
-#else
 #define I2C_BUS_BMC 0x00
-#endif
 
 #if defined(CONFIG_BOARD_AST2700_DCSCM)
 #define I2C_BUS_PCH 0x05
